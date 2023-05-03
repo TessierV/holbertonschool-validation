2e012d9a6a1bf68cd5b7e4a165a635c6c12bc074
TessierV <5628@holbertonstudents.com>
2023-05-03 04:22:09 -0700

    test script 4

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -6,3 +6,9 @@ apt-get install -y make golang-go git curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugo.deb
 apt install ./hugo.deb
 make build
+if [ $? -eq 0 ]; then
+    exit 0
+else
+    exit 1
+fi
+
