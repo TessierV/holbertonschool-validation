1944445d99338bb66c8bb4c69d9eb353a517f658
TessierV <5628@holbertonstudents.com>
2023-05-15 10:51:38 -0700

    redo setup

---
+++ module3_task1/content/posts/welcome.md
@@ -0,0 +1,6 @@
+---
+title: "Welcome"
+date: 2023-05-15T10:38:16-07:00
+draft: true
+---
+
---
+++ module3_task1/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# install a good version of hugo
+apt-get update && apt-get install -y make wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
+tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
+rm hugo.deb
