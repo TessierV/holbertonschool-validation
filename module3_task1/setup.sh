b74244fd258335a26c1c337839661364b013ce41
TessierV <5628@holbertonstudents.com>
2023-05-22 00:15:15 -0700

    test check

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 # Install a good version of Hugo
-apt-get update && apt-get install -y make wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
-tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
-rm hugo.deb
+apt-get update && apt-get install -y hugo make
+make build
