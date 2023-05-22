a840b8a8b8acc490dc261c692443926ba9549694
TessierV <5628@holbertonstudents.com>
2023-05-22 00:42:23 -0700

    test setup

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-# Install a good version of Hugo
-apt-get update && apt-get install -y hugo make
-make build
+apt-get update && apt-get install -y make git
+
+# download/install minimal version to use template ananke
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
+apt install ./hugo.deb
+#remove file after installation
+rm hugo.deb
+
+# download/install linter
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.52.2
+npm install -g markdownlint-cli
+npm install -g markdown-link-check
